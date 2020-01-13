687e61d53199ea2ce73ebbace27d45a7d7507fe9
vuphu <vuphu.itus@gmail.com>
2020-01-13 11:06:35 +0700

    # fix #3

--- src/app/src/main/java/com/example/advancedandroid/ImageRecyclerViewAdapter.kt
+++ src/app/src/main/java/com/example/advancedandroid/ImageRecyclerViewAdapter.kt
@@ -2,9 +2,6 @@ package com.example.advancedandroid
 
 import android.app.Activity
 import android.content.Context
-import android.content.pm.PackageManager
-import android.graphics.BitmapFactory
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -17,20 +14,16 @@ import com.bumptech.glide.Glide
 class ImageRecyclerViewAdapter(private val context: Context, private val data: MutableList<String>)
     : RecyclerView.Adapter<ImageRecyclerViewAdapter.ViewHolder>() {
 
-    private var spanCount: Int = 1
+    private var mSpanCount: Int = 1
     private var displayMetrics = Utils.getDisplayMetrics(context as Activity)
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imageView = itemView.findViewById<ImageView>(R.id.imgImage)
-    }
-
-    fun addData(data: MutableList<String>) {
+    fun addData(data: List<String>) {
         this.data.addAll(data)
         notifyItemRangeChanged(this.data.size - data.size, data.size)
     }
 
     fun setSpanCount(spanCount: Int) {
-        this.spanCount = spanCount
+        this.mSpanCount = spanCount
         notifyDataSetChanged()
     }
 
@@ -44,10 +37,22 @@ class ImageRecyclerViewAdapter(private val context: Context, private val data: M
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val size = displayMetrics.widthPixels / spanCount
-        val params = LinearLayout.LayoutParams(size, size)
-        holder.imageView.layoutParams = params
-        Glide.with(context).load(data[position])
-            .into(holder.imageView);
+        holder.bind(data[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imageView = itemView.findViewById<ImageView>(R.id.imgImage)
+
+        private fun loadImage(uri: String, imageView: ImageView) {
+            Glide.with(context).load(uri)
+                .into(imageView)
+        }
+
+        fun bind(uri: String) {
+            val size = displayMetrics.widthPixels / mSpanCount
+            val params = LinearLayout.LayoutParams(size, size)
+            imageView.layoutParams = params
+            loadImage(uri, imageView)
+        }
     }
 }
